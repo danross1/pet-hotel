8c24b3d3ebd72bf0703b21cfdf662544c3d17082
Dan Ross <dr.ross15@gmail.com>
2018-06-14 14:50:40 -0500

    add form to dashboard.html

--- server/public/scripts/controllers/dashboard.controller.js
+++ server/public/scripts/controllers/dashboard.controller.js
@@ -1,3 +1,9 @@
 app.controller('DashboardController', ['HotelService', function(HotelService){
     let self = this;
+    
+    HotelService.getOwners().then(function(){
+        self.owners = HotelService.owners.list;
+        console.log(self.owners);
+        
+    })
 }]);
--- server/public/scripts/services/hotel.service.js
+++ server/public/scripts/services/hotel.service.js
@@ -1,3 +1,16 @@
 app.service('HotelService', ['$http', function($http){
     let sv = this;
+    sv.owners = {list: [] }
+
+    sv.getOwners = function(){
+        return $http({
+            method: 'GET',
+            url: '/owner'
+        }).then(function(response){
+            console.log('response from GET owners:', response.data);
+            sv.owners.list = response.data;
+        })
+    }
+
+    sv.getOwners();
 }]);
