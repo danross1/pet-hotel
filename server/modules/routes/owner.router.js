ea672781d2f20b639e968582830c1c0c52d964aa
Dan Ross <dr.ross15@gmail.com>
2018-06-14 11:39:55 -0500

    add owner post route server-side

--- server/modules/routes/owner.router.js
+++ server/modules/routes/owner.router.js
@@ -22,9 +22,25 @@ router.get('/', (req, res)=>{
 });
 
 router.post('/', (req, res)=>{
-    console.log('in POST owner');
-    res.sendStatus(200);
-    
+    pool.connect(function (errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            console.log('error', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            let ownerName = req.body.name;
+            const queryText = `INSERT INTO owners (name)
+                VALUES($1)`;
+            client.query(queryText, [ownerName], function (errorMakingDatabaseQuery, result) {
+                done();
+                if (errorMakingDatabaseQuery) {
+                    console.log('error', errorMakingDatabaseQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
 });
 
 router.put('/', (req, res)=>{
