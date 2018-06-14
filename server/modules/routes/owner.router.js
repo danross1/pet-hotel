3288304bdf93535248c55c03dbfd9bfa665ea7ed
Dan Ross <dr.ross15@gmail.com>
2018-06-14 13:15:05 -0500

    add owner put route backend

--- server/modules/routes/owner.router.js
+++ server/modules/routes/owner.router.js
@@ -50,31 +50,18 @@ router.put('/:id', (req, res)=>{
             console.log('error', errorConnectingToDatabase);
             res.sendStatus(500);
         } else {
-            let powerID = req.params.id;
-            let powerName = req.body.name || null;
-            let powerDescription = req.body.description || null;
-            let powerInjection = [];
-            if(powerName && powerDescription) {
-                queryText = `UPDATE power SET name=$1, description=$2 WHERE id=$3`;
-                powerInjection = [powerName, powerDescription, powerID];
-            } else if(!powerName){
-                queryText = `UPDATE power SET description=$1 WHERE id=$2`;
-                powerInjection = [powerDescription, powerID];
-            } else if(!powerDescription) {
-                queryText = `UPDATE power SET name=$1 WHERE id=$2`;
-                powerInjection = [powerName, powerID];
-            }
-            console.log('queryText:', queryText);
-            console.log('powerInjection:', powerInjection);
+            let ownerID = req.params.id;
+            let ownerName = req.body.name || null;
+            let ownerInjection = [ownerName, ownerID];
+            let queryText = 'UPDATE owners SET name=$1 WHERE id=$2';
             
-            
-            client.query(queryText, powerInjection, function (errorMakingDatabaseQuery, result) {
+            client.query(queryText, ownerInjection, function (errorMakingDatabaseQuery, result) {
                 done();
                 if (errorMakingDatabaseQuery) {
                     console.log('error', errorMakingDatabaseQuery);
                     res.sendStatus(500);
                 } else {
-                    res.sendStatus(201);
+                    res.sendStatus(200);
                 }
             });
         }
