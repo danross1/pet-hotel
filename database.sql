2408d8ae40506741820c7197e7ce5da74bfacb7d
Dan Ross <dr.ross15@gmail.com>
2018-06-14 11:05:03 -0500

    add database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+CREATE TABLE pets (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	breed VARCHAR(100) NOT NULL,
+	color VARCHAR(20) NOT NULL,
+	dateCheckedIn DATE
+);
